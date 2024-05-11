6e6077b0fd68739b18cad34eba277b70fa8a6d8f
gurka <gurkab@gmail.com>
2024-05-11 08:59:31 -0700

    shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("com.github.johnrengelman.shadow") version "7.1.0"
     id("com.diffplug.spotless") version "6.24.0"
 }
 
@@ -17,6 +19,16 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("com.gurkab.fellowshipinterviewscheduler.Application")
+}
+
+tasks.jar {
+    manifest {
+        attributes("Main-Class" to "com.gurkab.fellowshipinterviewscheduler.Application")
+    }
 }
 
 tasks.test {
